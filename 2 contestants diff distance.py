9384db138487118594ca54e7b4c8e691adb593ca
Siva Nookala <siva.nookala@gmail.com>
2023-05-09 23:31:31 +0530

    Renamed file to two_contestants_with_different_distance.py for Races

--- 2 contestants diff distance.py
+++
@@ -1,9 +0,0 @@
-A = float(input("A runs a distance ="))
-B = float(input("B runs a distance ="))
-distance = (B/A)
-print(f'distance = {B/A}')
-
-dif_distance = float(input("B beat A in how much distance ="))
-B_beat_A = dif_distance-(dif_distance/distance)
-print(f'B beat A = {dif_distance}- {dif_distance}/{distance}')
-print("B beats by A =" + str(B_beat_A) + "m")
