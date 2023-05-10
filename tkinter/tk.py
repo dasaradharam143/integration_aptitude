87e6ae2efb5f507a476a72be7f6c1dfea820ed1f
Dasaradh <dasarathram143@gmail.com>
2023-05-10 23:58:49 +0530

    commit

--- tkinter/tk.py
+++ tkinter/tk.py
@@ -11,7 +11,8 @@ def submit():
         numbers = function1(my_box.get(), my_box1.get())
         my_label.config(text=numbers)
     except:
-        my_label.config(text='enter valid numbers and power')
+        my_label.config(text='Enter valid numbers and power')
+
 
 my_label1 = Label(root, text='enter numbers', font=('Roboto', 10), bg='orange')
 my_label1.pack(pady=10)
@@ -27,13 +28,6 @@ my_box1.pack()
 my_label = Label(root, text='', font=('Roboto', 12), fg='black')
 my_label.pack(pady=10)
 
-#
-# my_box2 = Entry(root)
-# my_box2.pack()
-#
-# my_label3 = Label(root,text='', font=('Roboto',20)).pack()
-# my_box3 = Entry(root)
-# my_box3.pack()
 
 # calling submit function
 my_button = Button(root, text="Submit", font=('roboto', 10), command=submit)
