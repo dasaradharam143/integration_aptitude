81d3ae0d1abf42aedf4898261a84493da1c538ab
Dasaradh <dasarathram143@gmail.com>
2023-05-10 20:32:32 +0530

    tkinter

--- tkinter/demo.py
+++ tkinter/demo.py
@@ -1,2 +1,13 @@
-import tkinter as tk
-from tkinter import *
+def power_of_n(a, n):
+    b = []
+    for i in a:
+        b.append(i ** n)
+    return b
+
+
+numbers = list(map(int, input('enter the numbers = ').split()))
+power = int(input('enter the power of n = '))
+total = power_of_n(numbers, power)
+print('total = ' + str(total))
+avg = sum(total) / len(total)
+print('average = ' + str(sum(total)) + '/' + str(len(total)) + ' = ' + str(avg))
