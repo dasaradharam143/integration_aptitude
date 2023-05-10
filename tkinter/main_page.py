b2a67520ec93daf04aa374f0d990df6097808452
Dasaradh <dasarathram143@gmail.com>
2023-05-10 22:26:49 +0530

    commit

--- tkinter/main_page.py
+++ tkinter/main_page.py
@@ -5,18 +5,16 @@ root = tk.Tk()
 root.title('Aptitude')
 root.geometry('600x600')
 
-label = Label(root,text='Aptitude Topics',font=('bold',18))
+label = Label(root, text='Aptitude Topics', font=('bold', 18))
 label.pack(pady=10)
 
-button1 = Button(root,text = '1.Averages',font=('bold,8'),bg='sky blue')
+button1 = Button(root, text='1.Averages', font=('roboto', 10), bg='sky blue')
 button1.pack(pady=8)
 
-button2 = Button(root,text = '2.Races and Games',font=('bold,8'),bg='sky blue')
+button2 = Button(root, text='2.Races and Games', font=('roboto', 10), bg='sky blue')
 button2.pack(pady=8)
 
-button3 = Button(root,text = '3.Stocks and Shares',font=('bold,8'),bg='sky blue')
+button3 = Button(root, text='3.Stocks and Shares', font=('roboto', 10), bg='sky blue')
 button3.pack(pady=8)
-
-
 
 root.mainloop()
