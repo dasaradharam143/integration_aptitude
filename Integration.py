fd7e81285b099488f640a36f82749a1503095642
Rakesh Bathini <rakeshbathini07@gmail.com>
2023-05-10 14:09:47 +0530

    commit

--- Integration.py
+++ Integration.py
@@ -1,11 +1,13 @@
 from subprocess import call
 
+
 def option():
-    condition = input('1)2_contestants different distance 2)2_and_3_contestants for finding time '
-                  '\n3)2_contestants to find distance by time 4)3_contestants same distance '
-                  '\n5)3_contestants with 2 distances 6)3_different distances  7)finding distance by speed ratios'
-                  '\n 8)finding 2 speeds 9) to find one speed  10) members game'
-                  '\n11)to find loss or win percent   PRESS 0 TO EXIT enter option = ')
+    condition = input('1)two_contestants_different_distance 2)two_and_three_contestants_for_finding_time '
+                      '\n3)two_contestants_to_find_distance_by_time 4)three_contestants_same_distance '
+                      '\n5)three_contestants_with_two_distances 6)three_different_distances '
+                      '\n7)finding_distance_speed ratios 8)finding_two_speeds 9) to_find_one_speed'
+                      '\n 10) members_game 11)to_find_loss_or_win_percent'
+                      '\n PRESS_0_TO_EXIT_enter_option = ')
 
     value = ''
     if condition == '1':
@@ -37,9 +39,10 @@ def option():
 
 
 def openpyfile():
-    call(['python',option()])
+    call(['python', option()])
     print()
     print('/////////////////////////////////////////////////////////')
     openpyfile()
+
 
 openpyfile()
