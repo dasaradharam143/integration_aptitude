786b672431171e3c868177bba47a66854fd1ea98
D.RAJKUMAR <rajkumar23dev@gmail.com>
2023-05-11 16:57:45 +0530

    find_income_on_investment in stocks and shares added

--- tkinter/find_income_on_investment.py
+++ tkinter/find_income_on_investment.py
@@ -5,37 +5,37 @@ root.title('find_investment_on_income')
 root.geometry('600x600')
 
 
-def find_investment_on_income(val1, val2, val3):
+def find_income_on_investment(val1, val2, val3):
     output = []
-    income1 = int(val1)
-    investment1 = int(val2)
-    income2 = int(val3)
-    investment2 = float((investment1 * income2) / income1)
+    investment1 = int(val1)
+    income1 = int(val2)
+    investment2 = int(val3)
+    income2 = float((income1 * investment2) / investment1)
     output.append(f'({val2} * {val3}) / {val1}')
-    output.append(f'\nInvestment2 or Stock2 : {investment2} ')
+    output.append(f'\nincome2 : {income2} ')
     return output
 
 
 def submit():
     try:
-        value = find_investment_on_income(my_box1.get(), my_box2.get(), my_box3.get())
+        value = find_income_on_investment(my_box1.get(), my_box2.get(), my_box3.get())
         my_label.config(text=' '.join(value))
 
     except:
         my_label.config(text='Enter valid numbers')
 
 
-my_label1 = Label(root, text='Income1 or Earn1 or obtain1', font=('Roboto', 10), bg='orange')
+my_label1 = Label(root, text='investment1', font=('Roboto', 10), bg='orange')
 my_label1.pack(pady=10)
 my_box1 = Entry(root)
 my_box1.pack()
 
-my_label2 = Label(root, text='Investment1 or Stock1', font=('Roboto', 10), bg='orange')
+my_label2 = Label(root, text='income1', font=('Roboto', 10), bg='orange')
 my_label2.pack(pady=10)
 my_box2 = Entry(root)
 my_box2.pack()
 
-my_label3 = Label(root, text='Income2 or Earn2 or obtain2', font=('Roboto', 10), bg='orange')
+my_label3 = Label(root, text='investment2', font=('Roboto', 10), bg='orange')
 my_label3.pack(pady=10)
 my_box3 = Entry(root)
 my_box3.pack()
--- tkinter/stock_value.py
+++ tkinter/stock_value.py
@@ -4,23 +4,24 @@ root = Tk()
 root.title('find_stock_value on selling_price')
 root.geometry('600x600')
 
-def find_stock_value(val1,val2):
+
+def find_stock_value(val1, val2):
     output = []
     selling_price = int(val1)
     brokerage = int(val2)
     stock_value = float(selling_price - brokerage)
-    output.append(f'{val1}-{val2}')
+    output.append(f'{val1} - {val2}')
     output.append(f'\nstock_value : {stock_value} ')
     return output
+
 
 def submit():
     try:
         value = find_stock_value(my_box1.get(),my_box2.get())
-        my_label.config(text=value)
+        my_label.config(text=' '.join(value))
+
     except:
         my_label.config(text='Enter valid numbers')
-
-
 
 
 my_label1 = Label(root, text='selling_price', font=('Roboto', 10), bg='orange')
@@ -33,11 +34,10 @@ my_label2.pack(pady=10)
 my_box2 = Entry(root)
 my_box2.pack()
 
-#//////////////////////////////////////////////////////////////////////
+# //////////////////////////////////////////////////////////////////////
 # creating a label
 my_label = Label(root, text='', font=('Roboto', 12), fg='black')
 my_label.pack(pady=10)
-
 
 # calling submit function
 my_button = Button(root, text="Submit", font=('roboto', 10), command=submit)
