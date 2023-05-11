8ada8542d1e4342ca82b67b9a274ba8894902bca
Dasaradh <dasarathram143@gmail.com>
2023-05-11 16:12:13 +0530

    label name change

--- tkinter/mean_average_tk.py
+++ tkinter/mean_average_tk.py
@@ -1,7 +1,7 @@
 from tkinter import *
 
 root = Tk()
-root.title('average of sum of n powers of numbers')
+root.title('mean average of numbers')
 root.geometry('600x600')
 
 
@@ -20,15 +20,15 @@ def submit():
         value = find_mean_average(my_box.get(), my_box1.get())
         my_label.config(text=value)
     except:
-        my_label.config(text='Enter valid numbers and power')
+        my_label.config(text='Enter valid numbers')
 
 
-my_label1 = Label(root, text='enter numbers', font=('Roboto', 10), bg='orange')
+my_label1 = Label(root, text='enter denominations', font=('Roboto', 10), bg='orange')
 my_label1.pack(pady=10)
 my_box = Entry(root)
 my_box.pack()
 
-my_label2 = Label(root, text='enter power of n', font=('Roboto', 10), bg='orange')
+my_label2 = Label(root, text='enter values', font=('Roboto', 10), bg='orange')
 my_label2.pack(pady=10)
 my_box1 = Entry(root)
 my_box1.pack()
