3acb46b8400fc2eaf01d34a92f42093256ab40bf
D.RAJKUMAR <rajkumar23dev@gmail.com>
2023-05-12 12:07:15 +0530

    find_ratios_of_investments is added to stock and shares

---
+++ tkinter/find_ratios_of_investments.py
@@ -0,0 +1,60 @@
+from tkinter import *
+from fractions import Fraction
+
+root = Tk()
+root.title('find_ratios_of_investments')
+root.geometry('600x600')
+
+
+def find_ratios_of_investments(val1, val2, val3, val4):
+    output = []
+    stock_value_a = Fraction(val1)
+    dividend_a = Fraction(val2)
+    stock_value_b = Fraction(val3)
+    dividend_b = Fraction(val4)
+    investment_a = (stock_value_a / dividend_a)  # take 1'rupee is income of both a and b stock value then
+    investment_b = stock_value_b / dividend_b  # their investment is stock_value / dividend equals income of 1'rupee
+    ratios_of_investments = investment_a / investment_b
+    output.append(f'\nRatios of Investments : {ratios_of_investments}')
+    return output
+
+
+def submit():
+    try:
+        value = find_ratios_of_investments(my_box1.get(), my_box2.get(), my_box3.get(), my_box4.get())
+        my_label.config(text=' '.join(value))
+
+    except Exception as err:
+        my_label.config(text=f'Enter valid numbers\nUnexpected {err=}')
+
+
+my_label1 = Label(root, text='stock_value_a', font=('Roboto', 10), bg='orange')
+my_label1.pack(pady=10)
+my_box1 = Entry(root)
+my_box1.pack()
+
+my_label2 = Label(root, text='dividend_a', font=('Roboto', 10), bg='orange')
+my_label2.pack(pady=10)
+my_box2 = Entry(root)
+my_box2.pack()
+
+my_label3 = Label(root, text='stock_value_b', font=('Roboto', 10), bg='orange')
+my_label3.pack(pady=10)
+my_box3 = Entry(root)
+my_box3.pack()
+
+my_label4 = Label(root, text='dividend_b', font=('Roboto', 10), bg='orange')
+my_label4.pack(pady=10)
+my_box4 = Entry(root)
+my_box4.pack()
+
+# //////////////////////////////////////////////////////////////////////
+# creating a label
+my_label = Label(root, text='', font=('Roboto', 12), fg='black')
+my_label.pack(pady=10)
+
+# calling submit function
+my_button = Button(root, text="Submit", font=('roboto', 10), command=submit)
+my_button.pack(pady=20)
+
+root.mainloop()
