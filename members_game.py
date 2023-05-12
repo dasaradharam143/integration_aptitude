8a8a948b4105ee59730c118dc28f185c42b6a8b7
Dasaradh <dasarathram143@gmail.com>
2023-05-12 12:46:25 +0530

    input change

--- members_game.py
+++ members_game.py
@@ -1,9 +1,9 @@
-a = list(map(int,input().split()))
+a = list(map(float, input().split()))
 length = len(a)
 total = 0
 for i in range(length):
     for j in range(length):
-        total = a[j]/2
-        a[j] = a[j]/2
-        a[length-i-1] += total
+        total = a[j] / 2
+        a[j] = round(a[j], 3) / 2
+        a[length - i - 1] += round(total, 3)
     print(a)
